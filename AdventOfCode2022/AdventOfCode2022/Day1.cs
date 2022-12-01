6005ead3d2371e4135ee92604c38faaf1dfb6bf1
Josh Roberts <robertjosh3@ecs.vuw.ac.nz>
2022-12-01 19:20:58 +1300

    day 1

---
+++ AdventOfCode2022/AdventOfCode2022/Day1.cs
@@ -0,0 +1,18 @@
+﻿//day 1 elves code https://adventofcode.com/2022/day/1
+
+var lines = File.ReadAllLines("input.txt");
+var elves = new List<int> {0};
+
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elves.Add(0);
+        continue;
+    }
+    var elf = int.Parse(line);
+    elves[^1] += elf;
+}
+
+var largest = elves.Max();
+Console.WriteLine(largest);

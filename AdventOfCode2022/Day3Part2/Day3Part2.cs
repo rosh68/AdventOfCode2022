7168ca75049daae24c133e8a6f9e670f381a1fca
Josh Roberts <robertjosh3@ecs.vuw.ac.nz>
2022-12-04 00:32:45 +1300

    day 3 part 2

---
+++ AdventOfCode2022/Day3Part2/Day3Part2.cs
@@ -0,0 +1,30 @@
+﻿//day 3 elves code https://adventofcode.com/2022/day/3
+
+var lines = File.ReadAllLines("input.txt");
+
+var groups = new List<List<string>>();
+groups.Add(new List<string>());
+var counter = 0;
+foreach (var line in lines) {
+    if (counter >= 3) {
+        groups.Add(new List<string>());
+        counter = 0;
+    }
+
+    groups[^1].Add(line);
+    counter++;
+}
+
+var total = groups.Select(group => group[0].First(x => group[1].Contains(x) && group[2].Contains(x)))
+    .Select(common => CharToPriority(common)).Sum();
+
+
+Console.WriteLine(total);
+
+int CharToPriority(char input) {
+    if (char.IsUpper(input)) {
+        return input - 38;
+    }
+
+    return input - 96;
+}

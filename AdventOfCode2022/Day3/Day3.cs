170ee218f9980d3d5cb2a8e38801858fafc92492
Josh Roberts <robertjosh3@ecs.vuw.ac.nz>
2022-12-04 00:05:35 +1300

    day 3 part 1

--- AdventOfCode2022/Day3/Day3.cs
+++ AdventOfCode2022/Day3/Day3.cs
@@ -1,20 +1,20 @@
 ﻿//day 3 elves code https://adventofcode.com/2022/day/3
 
 var lines = File.ReadAllLines("input.txt");
-var total = 0;
+var total = (from line in lines
+    let half = line.Length / 2
+    let part1 = line[..half]
+    let part2 = line[half..]
+    select part1.First(x => part2.Contains(x))
+    into match
+    select CharToPriority(match)).Sum();
 
-foreach (var line in lines) {
-    var half = line.Length / 2;
-    var part1 = line[..half];
-    var part2 = line[half..];
-    
-    
-    //Console.WriteLine("part 1 " + part1 + " part 2 " + part2);
-}
+Console.WriteLine(total);
 
 int CharToPriority(char input) {
     if (Char.IsUpper(input)) {
         return input - 38;
     }
+
     return input - 96;
 }
